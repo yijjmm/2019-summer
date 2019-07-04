83c82f893e05849f037596504d29fb71d1d926a7
Jimin Lee <47144126+yijjmm@users.noreply.github.com>
2019-07-04 16:59:40 +0900

    Create 190704 표준체중 before.c

---
+++ 190704 표준체중 before.c
@@ -0,0 +1,78 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	print "키를 알려주세요.  "
+		input height
+
+		s_weight ←(height - 100) * 0.9
+
+		print "표준 체중: ", s_weight
+
+		return 0;
+}
+/* 1~100의 합 구하기
+	int i, n, sum;	// 변수 선언
+
+	printf("1~얼마까지의 합? ");
+	scanf("%d", &n);
+
+	i=1;
+	while (i <= 5)
+	{
+		sum = sum + i;
+		i = i + 1;
+	}
+
+	printf("1~%d의 합= %d", n,  sum);
+*/
+
+/* 0~100 중 3의 배수의 합 구하기
+	int i, n, sum;	// 변수 선언
+
+	printf("1~얼마까지의 3의 배수의 합? ");
+	scanf("%d", &n);
+
+	sum = sum + i;
+	i = i + 3;
+	i=0;
+	while (i <= n)
+	{
+
+	}
+
+	printf("0~%d의 3의 배수의 합= %d", n,  sum);
+
+*/
+
+
+/* 문제3 1)
+print “체중은?  “
+input weight
+
+①  print “키는?  “
+②  input height
+
+⑤  s_weight ← (height-100) * 0.9
+⑥  dif ← weight – s_weight
+
+⑦  print “표준 체중: “, s_weight
+⑧  print “차이: “, dif
+*/
+
+/* 문제3 2)
+ print “체중은?  “
+input weight
+
+s_weight ← (height-100) * 0.9
+
+①  print “키는?  “
+input height
+
+⑥  dif ← weight – s_weight
+
+⑦  print “표준 체중: “, s_weight
+⑧  print “차이: “, dif
+
+*/
