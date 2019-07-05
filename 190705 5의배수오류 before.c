2b99945e6c36856a795bd12df5968085ae76a2b0
Jimin Lee <47144126+yijjmm@users.noreply.github.com>
2019-07-05 16:13:48 +0900

    Create 190705 5의배수오류 before.c

---
+++ 190705 5의배수오류 before.c
@@ -0,0 +1,34 @@
+// 5의 배수 개수가 틀리게 나오는 오류를 수정하기
+
+#include <stdio.h>
+
+int main()								// main 함수 정의
+{
+    int i, n, sum, cnt3;
+	
+	printf("1부터 얼마까지의 3의 배수와 5의 배수 개수?");		// print
+	scanf("%d", &n);					// input
+	
+	cnt3=0;
+	cnt5=0;
+
+	i = 0;
+	while (i<=n)
+	{		
+		if (i % 3 == 0)				// 3으로 나눈 나머지가 0과 같다면(==)
+		{
+			cnt3 = cnt3 + 1;
+		}	
+		else						// 3으로 나눈 나머지가 0과 같지 않다면
+		{
+			cnt5 = cnt5 + 1;
+		}
+
+		i = i+1;
+	}
+	
+    printf("1~%d까지 3의 배수 개수 : %d \n", cnt3);	// print 3의 배수 개수
+	printf("1~%d까지 5의 배수 개수 : %d \n", cnt5);	// print 5의 배수 개수
+
+	return 0;
+}
